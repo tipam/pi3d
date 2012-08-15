37167f225135d796b931ac0f152e37ff050f4d0d
paddywwoof <patrick@eldwick.org.uk>
2012-08-15 21:55:13 +0000

    maze with overall texture and surface texture - needs pi3dCommon line 150 commenting out: glDisable(GL_DEPTH_TEST

--- amazing.py
+++ amazing.py
@@ -17,6 +17,13 @@ import pi3d,math,random,glob,time
 
 rads = 0.017453292512 # degrees to radians
 
+#helpful messages
+print "Esc to quit, W to go forward, Mouse to steer, Space to jump."
+print "At the edge you will turn into a ghost and be able to fly "
+print "and pass through rocks! There are limited numbers of jumps."
+print "Good turnings are often greener and tend to be near"
+print "(but in the opposite direction to) big holes"
+
 # Setup display and initialise pi3d
 display = pi3d.display()
 display.create3D(10,10,1200,700, 0.5, 800.0, 60.0) # x,y,width,height,near,far,aspect
@@ -26,7 +33,8 @@ display.setBackColour(0.4,0.8,0.8,1) # r,g,b,alpha
 texs=pi3d.textures()
 # Setting 2nd param to True renders 'True' Blending
 # (this can be changed later to 'False' with 'cloudimg.blend = False')
-rockimg = texs.loadTexture("textures/rock1.jpg")
+rockimg1 = texs.loadTexture("textures/techy1.jpg")
+rockimg2 = texs.loadTexture("textures/rock1.png", True)
 tree2img = texs.loadTexture("textures/tree2.png")
 raspimg = texs.loadTexture("textures/Raspi256x256.png")
 
@@ -37,9 +45,10 @@ myecube = pi3d.createEnvironmentCube(900.0,"CROSS")
 mapwidth=1000.0
 mapdepth=1000.0
 mapheight=80.0
-mymap = pi3d.createElevationMapFromTexture("textures/maze1.jpg",mapwidth,mapdepth,mapheight,128,128,64) #testislands.jpg
+mymap = pi3d.createElevationMapFromTexture("textures/maze1.jpg",mapwidth,mapdepth,mapheight,128,128,1)
+mymap2 = pi3d.createElevationMapFromTexture("textures/maze1.jpg",mapwidth,mapdepth,mapheight,128,128,64,"detail",0.0, 0.05, 0.0) 
 
-myfog = pi3d.fog(0.05, (0.1,0.1,0.1,1.0)) 
+myfog = pi3d.fog(0.02, (0.1,0.1,0.1,1.0)) 
 
 #Create tree models
 treeplane = pi3d.createPlane(4.0,5.0)
@@ -53,7 +62,7 @@ mytrees1 = pi3d.createMergeShape("trees1")
 mytrees1.cluster(treemodel1, mymap,0.0,0.0,900.0,900.0,30,"",8.0,3.0)
 # (shape,elevmap,xpos,zpos,w,d,count,options,minscl,maxscl)
 raspberry = pi3d.createMergeShape("rasp")
-raspberry.cluster(treemodel1, mymap,0.0,0.0,990.0,990.0,10,"",8.0,1.0)
+raspberry.cluster(treemodel1, mymap,250.250,0.0,470.0,470.0,10,"",8.0,1.0)
 
 light = pi3d.createLight(0, 10,10,10, "", 50,100,0)
 light.on()
@@ -62,7 +71,7 @@ light.on()
 scshots = 1
 
 #energy counter
-hp = 50
+hp = 25
 
 #avatar camera
 rot=0.0
@@ -81,11 +90,10 @@ mymouse.start()
 
 omx=mymouse.x
 omy=mymouse.y
-mxMin, mxMax, myMin, myMax = omx, omx, omy, omy
 
 lastTm = time.time()
-dt = 0.1 #time between frames for water
 m = pi3d.matrix()
+fly = False
 # Display scene and rotate cuboid
 while 1:
     display.clear()
@@ -97,21 +105,18 @@ while 1:
     
     myecube.draw(ectex,xm,ym,zm)
     myfog.on()
-    mymap.draw(rockimg)
+    #rockimg1.blend = False
+    mymap.draw(rockimg1)
+    mymap2.draw(rockimg2)
     mytrees1.drawAll(tree2img)
     raspberry.drawAll(raspimg)
     myfog.off()
     
     mx=mymouse.x
     my=mymouse.y
-    if mx > mxMax: mxMax = mx
-    if mx < mxMin: mxMin = mx
-    if my > myMax: myMax = my
-    if my < myMin: myMin = my
     
-    #if mx>display.left and mx<display.right and my>display.top and my<display.bottom:
-    rot += (mx-(mxMax + mxMin)/2.0)*0.004 + (mx - omx)*0.2
-    tilt -= (my-(mxMax + mxMin)/2.0)*0.004 + (my - omy)*0.2
+    rot += (mx-omx)*0.2
+    tilt -= (my-omy)*0.2
     omx=mx
     omy=my
 
@@ -119,46 +124,53 @@ while 1:
     k = mykeys.read()
     if k >-1:
         if k==119: #key W
-	    dx = -math.sin(rot*rads)
-	    dz = math.cos(rot*rads)
-	    dy = -(mymap.calcHeight(xm + dx, zm + dz)+avhgt) - ym
-	    if dy > -0.5:
-		xm += dx
-		zm += dz
-		ym += dy
+			dx = -math.sin(rot*rads)
+			dz = math.cos(rot*rads)
+			if (fly):
+				dy = math.sin(tilt*rads)
+				xm += dx
+				zm += dz
+				ym += dy
+			else:
+				dy = -(mymap.calcHeight(xm + dx, zm + dz)+avhgt) - ym
+				if dy > -0.5: # limit steepness so can't climb up walls
+					xm += dx
+					zm += dz
+					ym += dy
+			if (xm < -490 or xm > 490 or zm < -490 or zm > 490): fly = True
         elif k==115: #kry S
-	    dx = math.sin(rot*rads)
-	    dz = -math.cos(rot*rads)
-	    dy = -(mymap.calcHeight(xm + dx, zm + dz)+avhgt) - ym
-	    if dy > -0.5:
-		xm += dx
-		zm += dz
-		ym += dy
+			dx = math.sin(rot*rads)
+			dz = -math.cos(rot*rads)
+			dy = -(mymap.calcHeight(xm + dx, zm + dz)+avhgt) - ym
+			if dy > -0.5:
+				xm += dx
+				zm += dz
+				ym += dy
         elif k==39: #key '
             tilt -= 2.0
             print tilt
         elif k==47: #key /
-	    tilt += 2.0
+			tilt += 2.0
         elif k==97: #key A
-	    rot -= 2
+			rot -= 2
         elif k==100: #key D
-	    rot += 2
+			rot += 2
         elif k==112: #key P
-	    display.screenshot("critters3D"+str(scshots)+".jpg")
-	    scshots += 1
+			display.screenshot("critters3D"+str(scshots)+".jpg")
+			scshots += 1
         elif k==32 and hp > 0: #key SPACE
-	    dx = -math.sin(rot*rads)*5.0
-	    dz = math.cos(rot*rads)*5.0
-	    dy = -(mymap.calcHeight(xm + dx, zm + dz)+avhgt) - ym
-	    xm += dx
-	    zm += dz
-	    ym += dy
-	    hp -= 1
+			dx = -math.sin(rot*rads)*5.0
+			dz = math.cos(rot*rads)*5.0
+			dy = -(mymap.calcHeight(xm + dx, zm + dz)+avhgt) - ym
+			xm += dx
+			zm += dz
+			ym += dy
+			hp -= 1
         elif k==27: #Escape key
             display.destroy()
             mykeys.close()
             break
         #else:
-	    #print k
+        #print k
    
     display.swapBuffers()
