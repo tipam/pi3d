e0241e979e27eda78f245a3f0565e8687b434c65
Tom Ritchford <tom@swirly.com>
2012-11-06 04:17:26 +0000

    Extract TCone.py

--- pi3d/__init__.py
+++ pi3d/__init__.py
@@ -46,38 +46,6 @@ def loadECfiles(path, fname, textures):
   return [textures.loadTexture(f) for f in files]
 
 
-class createTCone(Shape):
-
-        def __init__(self,radiusBot=1.2,radiusTop=0.8,height=2.0,sides=12, name="",x=0.0,y=0.0,z=0.0, rx=0.0, ry=0.0, rz=0.0, sx=1.0, sy=1.0, sz=1.0, cx=0.0,cy=0.0,cz=0.0):
-                super(createTCone,self).__init__(name, x,y,z, rx,ry,rz, sx,sy,sz, cx,cy,cz)
-
-                if Constants.VERBOSE:
-                  print "Creating Truncated Cone ..."
-
-                path = []
-                path.append((0,height * .5))
-                path.append((radiusTop, height * .5))
-                path.append((radiusBot, -height * .5))
-                path.append((0,-height * .5))
-
-                self.radiusBot = radiusBot
-                self.radiusTop = radiusTop
-                self.height = height
-                self.sides = sides
-                self.ttype = GL_TRIANGLES
-
-                results = lathe(path, sides, True)
-
-                self.vertices = eglfloats(results[0])
-                self.normals = eglfloats(results[1])
-                self.indices = eglshorts(results[2])
-                self.tex_coords = eglfloats(results[3])
-                self.ssize = results[4]
-
-
-        def draw(self,tex=None):
-                shape_draw(self,tex)
-
 class createExtrude(Shape):
 
         def __init__(self, path, height=1.0, name="",x=0.0,y=0.0,z=0.0, rx=0.0, ry=0.0, rz=0.0, sx=1.0, sy=1.0, sz=1.0, cx=0.0,cy=0.0,cz=0.0):
