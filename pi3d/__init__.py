91c17812ba301d0831d9ac73cb58126c3e4ea88b
Tom Ritchford <tom@swirly.com>
2012-11-06 03:51:52 +0000

    Extract Sphere.py

--- pi3d/__init__.py
+++ pi3d/__init__.py
@@ -46,36 +46,6 @@ def loadECfiles(path, fname, textures):
   return [textures.loadTexture(f) for f in files]
 
 
-class createSphere(Shape):
-
-        def __init__(self,radius=1,slices=12,sides=12,hemi=0.0,name="", x=0.0,y=0.0,z=0.0, rx=0.0, ry=0.0, rz=0.0, sx=1.0, sy=1.0, sz=1.0, cx=0.0,cy=0.0,cz=0.0):
-                super(createSphere,self).__init__(name, x,y,z, rx,ry,rz, sx,sy,sz, cx,cy,cz)
-
-                print "Creating sphere ..."
-
-                path = []
-                st = (math.pi*(1.0-hemi))/slices
-                for r in range(0,slices+1):
-                    path.append((radius * math.sin(r*st),radius * math.cos(r*st)))
-
-                self.radius = radius
-                self.slices = slices
-                self.sides = sides
-                self.hemi = hemi
-                self.ttype = GL_TRIANGLES
-
-                results = lathe(path, sides, True)
-
-                self.vertices = eglfloats(results[0])
-                self.normals = eglfloats(results[1])
-                self.indices = eglshorts(results[2])
-                self.tex_coords = eglfloats(results[3])
-                self.ssize = results[4]
-
-        def draw(self,tex=None):
-                shape_draw(self,tex)
-
-
 class createTorus(Shape):
 
         def __init__(self,radius=2.0,thickness=0.5, ringrots=6, sides=12,name="", x=0.0,y=0.0,z=0.0, rx=0.0, ry=0.0, rz=0.0, sx=1.0, sy=1.0, sz=1.0, cx=0.0,cy=0.0,cz=0.0):
