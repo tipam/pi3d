d948c3bb353acc0f604d8a791f6ec4cafe2b30fb
Tom Ritchford <tom@swirly.com>
2012-11-06 05:36:31 +0000

    Cleaning up Ball.py

--- pi3d/Ball.py
+++ pi3d/Ball.py
@@ -17,35 +17,34 @@ class Ball(object):
     self.mass = radius * 2
     self.decay = decay
 
-  def hit(self,otherball):
+  def hit(self, otherball):
     #used for pre-checking ball positions
-    dx = (self.x+self.vx)-(otherball.x+otherball.vx)
-    dy = (self.y+self.vy)-(otherball.y+otherball.vy)
-    rd = self.radius+otherball.radius
-    if (dx**2+dy**2) <= (rd**2): return True
-    else: return False
+    dx = (self.x + self.vx) - (otherball.x + otherball.vx)
+    dy = (self.y + self.vy) - (otherball.y + otherball.vy)
+    rd = self.radius + otherball.radius
+    return (dx**2+dy**2) <= (rd**2)
 
   def collisionBounce(self,otherball):
-    dx = self.x-otherball.x
-    dy = self.y-otherball.y
-    rd = self.radius+otherball.radius
+    dx = self.x - otherball.x
+    dy = self.y - otherball.y
+    rd = self.radius + otherball.radius
 
-    if (dx**2+dy**2) <= (rd**2):
-      cangle = math.atan2(dy,dx)
-      mag1 = math.sqrt(self.vx**2+self.vy**2)
-      mag2 = math.sqrt(otherball.vx**2+otherball.vy**2)
-      dir1 = math.atan2(self.vy,self.vx)
-      dir2 = math.atan2(otherball.vy,otherball.vx)
-      nspx1 = mag1 * math.cos(dir1-cangle)
-      nspy1 = mag1 * math.sin(dir1-cangle)
-      nspx2 = mag2 * math.cos(dir2-cangle)
-      nspy2 = mag2 * math.sin(dir2-cangle)
-      fspx1 = ((self.mass-otherball.mass)*nspx1+(otherball.mass*2)*nspx2)/(self.mass+otherball.mass)
-      fspx2 = ((self.mass*2)*nspx1+(otherball.mass-self.mass)*nspx2)/(self.mass+otherball.mass)
+    if (dx ** 2 + dy **2) <= (rd ** 2):
+      cangle = math.atan2(dy, dx)
+      mag1 = math.sqrt(self.vx ** 2 + self.vy ** 2)
+      mag2 = math.sqrt(otherball.vx ** 2 + otherball.vy ** 2)
+      dir1 = math.atan2(self.vy, self.vx)
+      dir2 = math.atan2(otherball.vy, otherball.vx)
+      nspx1 = mag1 * math.cos(dir1 - cangle)
+      nspy1 = mag1 * math.sin(dir1 - cangle)
+      nspx2 = mag2 * math.cos(dir2 - cangle)
+      nspy2 = mag2 * math.sin(dir2 - cangle)
+      fspx1 = ((self.mass - otherball.mass) * nspx1 + (otherball.mass * 2) * nspx2) / (self.mass + otherball.mass)
+      fspx2 = ((self.mass * 2) * nspx1 + (otherball.mass - self.mass) * nspx2) / (self.mass + otherball.mass)
       fspy1 = nspy1
       fspy2 = nspy2
-      self.vx = math.cos(cangle)*fspx1+math.cos(cangle+math.pi*.5)*fspy1
-      self.vy = math.sin(cangle)*fspx1+math.sin(cangle+math.pi*.5)*fspy1
-      otherball.vx = math.cos(cangle)*fspx2+math.cos(cangle+math.pi*.5)*fspy2
-      otherball.vy = math.sin(cangle)*fspx2+math.sin(cangle+math.pi*.5)*fspy2
+      self.vx = math.cos(cangle) * fspx1 + math.cos(cangle + math.pi * .5) * fspy1
+      self.vy = math.sin(cangle) * fspx1 + math.sin(cangle + math.pi * .5) * fspy1
+      otherball.vx = math.cos(cangle) * fspx2 + math.cos(cangle + math.pi * .5) * fspy2
+      otherball.vy = math.sin(cangle) * fspx2 + math.sin(cangle + math.pi * .5) * fspy2
 
