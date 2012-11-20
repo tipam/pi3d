809b49c9fb14e308a4fbd5f24af1a93114df6067
Tom Ritchford <tom@swirly.com>
2012-11-20 23:54:34 +0000

    New DisplayLoop, port CollisionBalls to it and fix up Ball to be a sprite

--- demos/CollisionBalls.py
+++ demos/CollisionBalls.py
@@ -1,114 +1,74 @@
-# Bouncing balls example using pi3d module
-# ========================================
-# Copyright (c) 2012 - Tim Skillman
-# Version 0.02 - 03Jul12
-#
-# This example does not reflect the finished pi3d module in any way whatsoever!
-# It merely aims to demonstrate a working concept in simplfying 3D programming on the Pi
-#
-# PLEASE INSTALL PIL imaging with:
-#
-#      $ sudo apt-get install python-imaging
-#
-# before running this example
-#
-# Bouncing demonstrates pi3d sprites over the desktop.
-# It uses the orthographic view scaled to the size of the window;
-# this means that sprites can be drawn at pixel resolution
-# which is more common for 2D.  Also demonstrates a mock title bar.
+import random
+import sys
 
-import sys, random
+from pi3d import Keyboard
 
 from pi3d.Display import Display
-from pi3d.Keyboard import Keyboard
+from pi3d.DisplayLoop import DisplayLoop
 from pi3d.Texture import Textures
 
 from pi3d.sprite.Ball import Ball
 
-from pi3d.util import Draw
-
 # Setup display and initialise pi3d
 display = Display()
-scnx=display.max_width
-scny=display.max_height
+scnx =  display.max_width
+scny = display.max_height
 display.create2D(0,0,scnx,scny,0)
 
 # Set last value (alpha) to zero for a transparent background!
 display.setBackColour(0,0.2,0.6,0)
 
-
 # Ball parameters
-maxballs = 15
-maxballsize = 100
-minballsize = 5
-maxspeed = 30
+MAX_BALLS = 15
+MIN_BALL_SIZE = 5
+MAX_BALL_SIZE = 100
+MAX_RANDOMIZATION_TRIES = 20
 
-texs=Textures()
+texs = Textures()
 balltex = []
 balltex.append(texs.loadTexture("textures/red_ball.png"))
 balltex.append(texs.loadTexture("textures/grn_ball.png"))
 balltex.append(texs.loadTexture("textures/blu_ball.png"))
 
-#create balls and positions and make sure they don't touch to start with
-balls = []
-b=0
-hit = True
-for b in range (0,maxballs):
+class RandomBall(Ball):
+  def __init__(self, texture):
+    super(RandomBall, self).__init__(texture, 1.0, 0, 0,
+                                     random.uniform(-10.0, 10.0),
+                                     random.uniform(-10.0, 10.0))
+    self.texture = texture
+    self.randomize()
 
-    r = random.random() * maxballsize+minballsize
-    x = random.random() * scnx
-    y = random.random() * scny
+  def randomize(self):
+    self.radius = random.uniform(MIN_BALL_SIZE, MAX_BALL_SIZE)
+    self.mass = self.radius * self.radius
+    self.x = random.uniform(0.0, scnx)
+    self.y = random.uniform(0.0, scny)
 
-    vx = random.random() * 20.0-10.0
-    vy = random.random() * 20.0-10.0
-    balls.append(Ball(r,x,y,vx,vy,0.0))
-    c=0
-    hit = False
-    while c < b and not hit:
-      Bb = balls[b]
-      if Bb.hit(balls[c]):
-        Bb.radius = random.random() * maxballsize + minballsize
-        Bb.x = random.random() * scnx
-        Bb.y = random.random() * scny
-        c=0
-        hit = True
+  def separate(self, balls, max_tries=MAX_RANDOMIZATION_TRIES):
+    for i in range(max_tries):
+      for b in balls:
+        if b.hit(self):
+          self.randomize()
+          break
       else:
-        c+=1
-        hit = False
+        return
+
+  def update(self, display_loop, index, t):
+    for ball in display_loop.sprites[0:index]:
+      self.bounce_collision(ball)
+    super(RandomBall, self).update(display_loop, index, t)
+
+  def load(self):
+    pass
 
 
-# Fetch key presses
-mykeys = Keyboard()
-scshots = 1
+# create balls and positions and make sure they don't touch to start with.
+balls = [RandomBall(balltex[0]) for b in range(MAX_BALLS)]
+for i, ball in enumerate(balls):
+  if i:
+    ball.separate(balls[0:i])
 
-print balls[0].x, balls[0].vx
+DisplayLoop(display,
+            check_for_close=Keyboard.make_closer(),
+            sprites=balls).loop()
 
-while True:
-  display.clear()
-
-  for b in range (0,len(balls)):
-    Bb = balls[b]
-    #check collisions with other balls and bounce if necessary
-    for c in range (b+1,len(balls)):
-      Bb.collisionBounce(balls[c])
-    Bb.x += Bb.vx
-    Bb.y += Bb.vy
-    #check if ball hits wall (to avoid getting stuck just off the edge)
-    if Bb.x > (scnx - Bb.radius): Bb.vx = -abs(Bb.vx)
-    elif Bb.x < Bb.radius: Bb.vx = abs(Bb.vx)
-    if Bb.y > (scny - Bb.radius): Bb.vy = -abs(Bb.vy)
-    elif Bb.y < Bb.radius: Bb.vy = abs(Bb.vy)
-
-    Draw.sprite(balltex[0], Bb.x, Bb.y, -1, Bb.radius, Bb.radius)
-
-  k = mykeys.read()
-  if k >-1:
-    if k==27:  #ESCAPE key
-      mykeys.close()
-      texs.deleteAll()
-      display.destroy()
-      break
-    if k==112:
-      display.screenshot("collisionballs.jpg")
-
-  display.swapBuffers()
--- pi3d/sprite/Ball.py
+++ pi3d/sprite/Ball.py
@@ -1,18 +1,23 @@
 import math
 
 from pi3d import *
+from pi3d.util import Draw
 from pi3d.util import Utility
 
 class Ball(object):
-  def __init__(self, radius, x, y, vx=0.0, vy=0.0, decay=0.001):
+  def __init__(self, texture, radius, x, y, vx=0.0, vy=0.0, decay=0.001):
+    self.texture = texture
     self.radius = radius
     self.x = x
     self.y = y
-    self.x = x
     self.vx = vx
     self.vy = vy
-    self.mass = radius ** 2
+    self.mass = radius * radius
     self.decay = decay
+
+  def move(self):
+    self.x += self.vx
+    self.y += self.vy
 
   def hit(self, otherball):
     #used for pre-checking ball positions
@@ -21,18 +26,21 @@ class Ball(object):
     rd = self.radius + otherball.radius
     return Utility.sqsum(dx, dy) <= (rd * rd)
 
-  def collisionBounce(self,otherball):
+  def bounce_collision(self, otherball):
     # relative positions
-    dx = self.x-otherball.x
-    dy = self.y-otherball.y
-    rd = self.radius+otherball.radius
+    dx = self.x - otherball.x
+    dy = self.y - otherball.y
+    rd = self.radius + otherball.radius
     # check sign of a.b to see if converging
-    dotP = Utility.dotproduct(dx, dy, 0, (self.vx - otherball.vx), (self.vy - otherball.vy), 0)
-    if dx**2+dy**2 <= rd**2 and dotP < 0:
+    dotP = Utility.dotproduct(dx, dy, 0,
+                              (self.vx - otherball.vx),
+                              (self.vy - otherball.vy), 0)
+    if dx * dx + dy * dy <= rd * rd and dotP < 0:
       R = otherball.mass/self.mass #ratio of masses
       D = dx/dy #glancing angle for equating angular momentum before and after collision
       #three more simultaneous equations for x and y components of momentum and k.e. give:
-      delta2y = 2 * (D*self.vx + self.vy - D*otherball.vx - otherball.vy)/(1 + D*D + D*D*R + R)
+      delta2y = 2 * (D * self.vx + self.vy - D * otherball.vx - otherball.vy) / (
+        1 + D * D + D * D * R + R)
       delta2x = D * delta2y
       delta1y = -1 * R * delta2y
       delta1x = -1 * R * D * delta2y
@@ -41,3 +49,20 @@ class Ball(object):
       self.vy += delta1y
       otherball.vx += delta2x
       otherball.vy += delta2y
+
+  def bounce_wall(self, width, height):
+    if self.x > (width - self.radius):
+      self.vx = -abs(self.vx)
+    elif self.x < self.radius:
+      self.vx = abs(self.vx)
+
+    if self.y > (height - self.radius):
+      self.vy = -abs(self.vy)
+    elif self.y < self.radius:
+      self.vy = abs(self.vy)
+
+  def update(self, display_loop, index, t):
+    self.move()
+    disp = display_loop.display
+    self.bounce_wall(disp.max_width, disp.max_height)
+    Draw.sprite(self.texture, self.x, self.y, -1, self.radius, self.radius)
