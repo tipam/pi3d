a4eedccc1ae53fb1db60631091d72ab2c0ca7cb7
paddywwoof <patrick@eldwick.org.uk>
2012-12-07 17:25:35 +0000

    earth + metalic orbiter

--- demos/Earth.py
+++ demos/Earth.py
@@ -19,6 +19,8 @@ from pi3d.Keyboard import Keyboard
 from pi3d.Texture import Texture
 
 from pi3d.context.Light import Light
+from pi3d.Camera import Camera
+from pi3d.Shader import Shader
 
 from pi3d.shape.Sphere import Sphere
 from pi3d.shape.Plane import Plane
@@ -27,8 +29,12 @@ from pi3d.util import Draw
 from pi3d.util.Screenshot import screenshot
 
 # Setup display and initialise pi3d
-DISPLAY = Display.create()
+DISPLAY = Display.create(x=50, y=50)
 DISPLAY.setBackColour(0,0,0,1)    	# r,g,b,alpha
+camera = Camera((0, 0, 0), (0, 0, -1), (1, 1000, DISPLAY.win_width/1000.0, DISPLAY.win_height/1000.0))
+light = Light((10, 10, -20))
+shader = Shader("shaders/bumpShade")
+#========================================
 
 # Setting 2nd param to True renders 'True' Blending
 # (this can be changed later to 'False' with 'cloudimg.blend = False')
@@ -38,11 +44,11 @@ moonimg = Texture("textures/moon.jpg")
 starsimg = Texture("textures/stars2.jpg")
 watimg = Texture("textures/water.jpg")
 
-mysphere = Sphere(2,24,24,0.0,"earth",0,0,-5.8)
-mysphere2 = Sphere(2.05,24,24,0.0,"clouds",0,0,-5.8)
-mymoon = Sphere(0.4,16,16,0.0,"moon",0,0,0)
-mymoon2 = Sphere(0.1,16,16,0.0,"moon2",0,0,0)
-myplane = Plane(50,50, "stars", 0,0,-10)
+mysphere = Sphere(camera, light, 2,24,24,0.0,"earth",0,0, 5.8)
+mysphere2 = Sphere(camera, light, 2.05,24,24,0.0,"clouds",0,0, 5.8)
+mymoon = Sphere(camera, light, 0.4,16,16,0.0,"moon",0,0,0)
+mymoon2 = Sphere(camera, light, 0.1,16,16,0.0,"moon2",0,0,0)
+myplane = Plane(camera, light, 50,50, "stars", 0,0, 10)
 
 # Fetch key presses
 mykeys = Keyboard()
@@ -54,32 +60,28 @@ m1Rad = 4 # radius of moon orbit
 m2Rad = 0.55 # radius moon's moon orbit
 
 
-#create a light
-mylight = Light(0,5,5,5,"sun",10,0,6, .1,.1,.2, 0)
-mylight.on()
-
 # Display scene
 while 1:
   DISPLAY.clear()
 
-  myplane.draw(starsimg)
+  myplane.draw(shader,[starsimg], 0.0, -1.0)
   myplane.rotateIncZ(0.01)
 
-  mysphere.draw(earthimg)
-  mysphere.rotateIncY(0.1)
-  mysphere2.draw(cloudimg)
-  mysphere2.rotateIncY(.15)
+  mysphere.draw(shader, [earthimg])
+  mysphere.rotateIncY(-0.1)
+  mysphere2.draw(shader, [cloudimg])
+  mysphere2.rotateIncY(-0.15)
 
-  mymoon.draw(moonimg)
+  mymoon.draw(shader, [moonimg, moonimg, starsimg], 1.0, 0.0)
   mymoon.position(mysphere.x+m1Rad*sin(rot1),mysphere.y+0,mysphere.z-m1Rad*cos(rot1))
-  mymoon.rotateIncY(0.2)
+  mymoon.rotateIncY(-0.2)
 
-  mymoon2.draw(watimg)
-  mymoon2.position(mymoon.x+m2Rad*sin(rot2),mymoon.y+0,mymoon.z-m2Rad*cos(rot2))
-  mymoon2.rotateIncY(3)
+  mymoon2.draw(shader, [watimg, watimg, starsimg], 4.0, 0.8)
+  mymoon2.position(mymoon.x - m2Rad*sin(rot2), mymoon.y, mymoon.z + m2Rad*cos(rot2))
+  mymoon2.rotateIncY(-5.0)
 
-  rot1 -= 0.005
-  rot2 -= 0.021
+  rot1 += 0.005
+  rot2 += 0.021
 
   k = mykeys.read()
   if k >-1:
