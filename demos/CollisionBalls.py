305964278573fa9fe9880d3f3decd5238274ac68
Tom Ritchford <tom@swirly.com>
2013-01-02 20:07:44 +0000

    Tiny cleanups to demos/CollisionBalls.py

--- demos/CollisionBalls.py
+++ demos/CollisionBalls.py
@@ -11,28 +11,28 @@ from pi3d.context.Light import Light
 
 from pi3d.sprite.Ball import Ball
 
-# Ball parameters
+# Ball parameters - you can change these
 MAX_BALLS = 15
 MIN_BALL_SIZE = 5
 MAX_BALL_SIZE = 100
+MAX_BALL_VELOCITY = 10.0
+
+KEYBOARD = Keyboard()
 
 BACKGROUND = 1.0, 1.0, 1.0, 0.0
+DISPLAY = Display.create(is_3d=False, background=BACKGROUND)
+WIDTH, HEIGHT = DISPLAY.win_width, DISPLAY.win_height
+
+CAMERA_LENS = (1, 1000, WIDTH / 1000.0, HEIGHT / 1000.0)
+CAMERA = Camera((0, 0, 0), (0, 0, -0.1), CAMERA_LENS)
+
+LIGHT = Light((10, 10, -20))
+
+SHADER = Shader('shaders/uv_flat')
 
 TEXTURE_NAMES = ['textures/red_ball.png',
                  'textures/grn_ball.png',
                  'textures/blu_ball.png']
-
-
-DISPLAY = Display.create(is_3d=False, background=BACKGROUND)
-WIDTH, HEIGHT = DISPLAY.win_width, DISPLAY.win_height
-
-KEYBOARD = Keyboard()
-
-CAMERA = Camera((0, 0, 0), (0, 0, -0.1),
-                (1, 1000, WIDTH / 1000.0, HEIGHT / 1000.0))
-LIGHT = Light((10, 10, -20))
-SHADER = Shader('shaders/uv_flat')
-
 TEXTURES = [Texture(t) for t in TEXTURE_NAMES]
 
 def random_ball():
@@ -42,8 +42,8 @@ def random_ball():
               radius=random.uniform(MIN_BALL_SIZE, MAX_BALL_SIZE),
               x=random.uniform(-WIDTH / 2.0, WIDTH / 2.0),
               y=random.uniform(-HEIGHT / 2.0, HEIGHT / 2.0),
-              vx=random.uniform(-10.0, 10.0),
-              vy=random.uniform(-10.0, 10.0))
+              vx=random.uniform(-MAX_BALL_VELOCITY, MAX_BALL_VELOCITY),
+              vy=random.uniform(-MAX_BALL_VELOCITY, MAX_BALL_VELOCITY))
 
 
 SPRITES = [random_ball() for b in range(MAX_BALLS)]
