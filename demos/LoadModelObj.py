0842b75433054ebb89fee921bfd3e657117e28df
Tom Ritchford <tom@swirly.com>
2013-01-03 04:06:06 +0000

    Fix LoadModelObj demo to use the default instance of Camera

--- demos/LoadModelObj.py
+++ demos/LoadModelObj.py
@@ -2,7 +2,6 @@ from pi3d import Display
 from pi3d.Keyboard import Keyboard
 from pi3d.Texture import Texture
 
-from pi3d.Camera import Camera
 from pi3d.Shader import Shader
 from pi3d.context.Light import Light
 
@@ -15,7 +14,6 @@ DISPLAY = Display.create(x=100, y=100)
 DISPLAY.set_background(r=0.2, g=0.4, b=0.6, alpha=1)
 
 
-camera = Camera((0, 0, 0), (0, 0, -1), (1, 1000, DISPLAY.width/1000.0, DISPLAY.height/1000.0))
 light = Light((10, 10, -20))
 shader = Shader("shaders/uv_reflect")
 #========================================
@@ -23,9 +21,9 @@ shader = Shader("shaders/uv_reflect")
 bumptex = Texture("textures/floor_nm.jpg")
 shinetex = Texture("textures/stars.jpg")
 
-
 # load model_loadmodel
-mymodel = Model(camera, light, 'models/teapot.obj' ,'teapot', 0, 0, 4)
+mymodel = Model(light=light, file_string='models/teapot.obj',
+                name='teapot', z=4)
 mymodel.set_shader(shader)
 mymodel.set_normal_shine(bumptex, 16.0, shinetex, 0.5)
 
