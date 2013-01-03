f19b4e371ec90acda541a66573d9d9e672ace864
Tom Ritchford <tom@swirly.com>
2013-01-03 03:09:55 +0000

    Get some more demos to use the default instance of Camera

--- demos/LoadModel.py
+++ demos/LoadModel.py
@@ -18,7 +18,6 @@ from pi3d import Display
 from pi3d.Keyboard import Keyboard
 from pi3d.Texture import Texture
 
-from pi3d.Camera import Camera
 from pi3d.Shader import Shader
 from pi3d.context.Light import Light
 
@@ -27,11 +26,8 @@ from pi3d.util.Matrix import Matrix
 from pi3d.util.Screenshot import screenshot
 
 # Setup display and initialise pi3d
-DISPLAY = Display.create(x=100, y=100)
-DISPLAY.set_background(r=0.2, g=0.4, b=0.6, alpha=1)
+DISPLAY = Display.create(x=100, y=100, background=(0.2, 0.4, 0.6, 1))
 
-
-camera = Camera((0, 0, 0), (0, 0, -1), (1, 1000, DISPLAY.width/1000.0, DISPLAY.height/1000.0))
 light = Light((1, 1, 1))
 shader = Shader("shaders/mat_reflect")
 #========================================
@@ -39,9 +35,9 @@ shader = Shader("shaders/mat_reflect")
 bumptex = Texture("textures/floor_nm.jpg")
 shinetex = Texture("textures/stars.jpg")
 
-
 # load model_loadmodel
-mymodel = Model(camera, light, 'models/teapot.egg' ,'teapot', 0, -2, 8)
+mymodel = Model(light=light, file_string='models/teapot.egg',
+                name='teapot', x=0, y=-2, z=8)
 mymodel.set_shader(shader)
 # material is set in the file
 mymodel.set_normal_shine(bumptex, 4.0, shinetex, 0.2, is_uv = False)
@@ -49,9 +45,7 @@ mymodel.set_normal_shine(bumptex, 4.0, shinetex, 0.2, is_uv = False)
 # Fetch key presses
 mykeys = Keyboard()
 
-while 1:
-  DISPLAY.clear()
-
+while DISPLAY.loop_running():
   mymodel.draw()
   mymodel.rotateIncY(2.0)
   mymodel.rotateIncZ(0.1)
@@ -59,13 +53,11 @@ while 1:
 
   k = mykeys.read()
   if k >-1:
-    if k==112:
+    if k == 112:
       screenshot('teapot.jpg')
     elif k==27:
       mykeys.close()
-      DISPLAY.destroy()
+      DISPLAY.close()
       break
     else:
       print k
-
-  DISPLAY.swapBuffers()
