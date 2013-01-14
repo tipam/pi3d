0440b502fd264861686d594394a40c95b2bb0301
Tom Ritchford <tom@swirly.com>
2013-01-14 16:28:05 -0500

    Now importing pi3d imports 'almost everything'

--- demos/CollisionBalls.py
+++ demos/CollisionBalls.py
@@ -5,41 +5,35 @@ import random
 import sys
 
 import demo
-
-from pi3d import Display
-from pi3d.Keyboard import Keyboard
-from pi3d.Shader import Shader
-from pi3d.Texture import Texture
-
-from pi3d.sprite.Ball import Ball
+import pi3d
 
 MAX_BALLS = 15
 MIN_BALL_SIZE = 5
 MAX_BALL_SIZE = 100
 MAX_BALL_VELOCITY = 10.0
 
-KEYBOARD = Keyboard()
+KEYBOARD = pi3d.Keyboard()
 
 BACKGROUND_COLOR = 1.0, 1.0, 1.0, 0.0
-DISPLAY = Display.create(is_3d=False, background=BACKGROUND_COLOR)
+DISPLAY = pi3d.Display.create(is_3d=False, background=BACKGROUND_COLOR)
 WIDTH, HEIGHT = DISPLAY.width, DISPLAY.height
 
-SHADER = Shader('shaders/uv_flat')
+SHADER = pi3d.Shader('shaders/uv_flat')
 
 TEXTURE_NAMES = ['textures/red_ball.png',
                  'textures/grn_ball.png',
                  'textures/blu_ball.png']
-TEXTURES = [Texture(t) for t in TEXTURE_NAMES]
+TEXTURES = [pi3d.Texture(t) for t in TEXTURE_NAMES]
 
 def random_ball():
   """Return a ball with a random color, position and velocity."""
-  return Ball(shader=SHADER,
-              texture=random.choice(TEXTURES),
-              radius=random.uniform(MIN_BALL_SIZE, MAX_BALL_SIZE),
-              x=random.uniform(-WIDTH / 2.0, WIDTH / 2.0),
-              y=random.uniform(-HEIGHT / 2.0, HEIGHT / 2.0),
-              vx=random.uniform(-MAX_BALL_VELOCITY, MAX_BALL_VELOCITY),
-              vy=random.uniform(-MAX_BALL_VELOCITY, MAX_BALL_VELOCITY))
+  return pi3d.Ball(shader=SHADER,
+                   texture=random.choice(TEXTURES),
+                   radius=random.uniform(MIN_BALL_SIZE, MAX_BALL_SIZE),
+                   x=random.uniform(-WIDTH / 2.0, WIDTH / 2.0),
+                   y=random.uniform(-HEIGHT / 2.0, HEIGHT / 2.0),
+                   vx=random.uniform(-MAX_BALL_VELOCITY, MAX_BALL_VELOCITY),
+                   vy=random.uniform(-MAX_BALL_VELOCITY, MAX_BALL_VELOCITY))
 
 
 SPRITES = [random_ball() for b in range(MAX_BALLS)]
--- pi3d/__init__.py
+++ pi3d/__init__.py
@@ -1,1 +1,45 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
 from pi3d.constants import *
+
+from pi3d import Display
+
+from pi3d.Camera import Camera
+from pi3d.Keyboard import Keyboard
+from pi3d.Mouse import Mouse
+from pi3d.Shader import Shader
+from pi3d.Texture import Texture
+
+from pi3d.context.Fog import Fog
+from pi3d.context.Light import Light
+
+from pi3d.shape import EnvironmentCube
+from pi3d.shape import Sprite
+
+from pi3d.shape.Cone import Cone
+from pi3d.shape.Cuboid import Cuboid
+from pi3d.shape.Cylinder import Cylinder
+from pi3d.shape.Disk import Disk
+from pi3d.shape.ElevationMap import ElevationMap
+from pi3d.shape.Extrude import Extrude
+from pi3d.shape.Helix import Helix
+from pi3d.shape.Lathe import Lathe
+from pi3d.shape.MergeShape import MergeShape
+from pi3d.shape.Model import Model
+from pi3d.shape.Plane import Plane
+from pi3d.shape.Shape import Shape
+from pi3d.shape.Sphere import Sphere
+from pi3d.shape.TCone import TCone
+from pi3d.shape.Torus import Torus
+
+from pi3d.sprite.Ball import Ball
+
+from pi3d.util import Draw
+from pi3d.util import Log
+from pi3d.util import String
+from pi3d.util import Utility
+
+from pi3d.util.Defocus import Defocus
+from pi3d.util.Font import Font
+from pi3d.util.Matrix import Matrix
+from pi3d.util.Ttffont import Ttffont
