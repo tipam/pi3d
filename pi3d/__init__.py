5ee7402632737e9e960d8dbb54d45eb7e1a1ee58
Tom Ritchford <tom@swirly.com>
2013-01-14 15:51:09 -0500

    Move libraries into constants

--- pi3d/__init__.py
+++ pi3d/__init__.py
@@ -1,19 +1,5 @@
-from echomesh.util import Log
-
 from pi3d.constants import *
 
+from echomesh.util import Log
 LOGGER = Log.logger(__name__)
-
-def _load_library(name):
-  """Try to load a shared library, report an error on failure."""
-  try:
-    import ctypes
-    return ctypes.CDLL('lib%s.so' % name)
-  except:
-    from echomesh.util import Log
-    Log.logger(__name__).error("Couldn't load library %s" % name)
-
-bcm = _load_library('bcm_host')
-opengles = _load_library('GLESv2')
-openegl = _load_library('EGL')
 
--- pi3d/constants/__init__.py
+++ pi3d/constants/__init__.py
@@ -1,4 +1,4 @@
-# List of constants
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 VERSION = '0.06'
 
@@ -21,10 +21,25 @@ from pi3d.constants.gl2 import *
 from pi3d.constants.gl2ext import *
 from pi3d.constants.gl import *
 
-# Define some extra constants that the automatic extraction misses
+# Define some extra constants that the automatic extraction misses.
 EGL_DEFAULT_DISPLAY = 0
 EGL_NO_CONTEXT = 0
 EGL_NO_DISPLAY = 0
 EGL_NO_SURFACE = 0
 DISPMANX_PROTECTION_NONE = 0
+
+# Lastly, load the libraries.
+def _load_library(name):
+  """Try to load a shared library, report an error on failure."""
+  try:
+    import ctypes
+    return ctypes.CDLL('lib%s.so' % name)
+  except:
+    from echomesh.util import Log
+    Log.logger(__name__).error("Couldn't load library %s" % name)
+    raise
+
+bcm = _load_library('bcm_host')
+opengles = _load_library('GLESv2')
+openegl = _load_library('EGL')
 
