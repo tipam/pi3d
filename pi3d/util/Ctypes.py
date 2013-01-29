02f2c4a9232b4491064048a175468ddcdadb086d
Tom Swirly <tom@swirly.com>
2013-01-29 17:15:49 -0500

    Ignore Mac OS/X .DS_Store files

--- pi3d/util/Ctypes.py
+++ pi3d/util/Ctypes.py
@@ -9,17 +9,22 @@ them to an external C library which wants ctypes.
 """
 
 def c_bytes(x):
+  """Return a tuple of c_byte, converted from a list of Python variables."""
   return (c_byte * len(x))(*x)
 
 def c_chars(x):
+  """Return a tuple of c_char, converted from a list of Python variables."""
   return (c_char * len(x))(*x)
 
 def c_floats(x):
   return (c_float * len(x))(*x)
+  """Return a tuple of c_float, converted from a list of Python variables."""
 
 def c_ints(x):
+  """Return a tuple of c_int, converted from a list of Python variables."""
   return (c_int * len(x))(*x)
 
 def c_shorts(x):
+  """Return a tuple of c_short, converted from a list of Python variables."""
   return (c_short * len(x))(*x)
 
