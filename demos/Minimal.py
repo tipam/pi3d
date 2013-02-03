aeecd4045a0f10b97d75864723cdfd3b8852b89d
paddywwoof <patrick@eldwick.org.uk>
2013-02-03 17:01:50 +0000

    some smartening up of star shader and demo in pi3d2.py

--- demos/Minimal.py
+++ demos/Minimal.py
@@ -1,10 +1,16 @@
 import demo
 import pi3d
 
-DISPLAY = pi3d.Display.create(w=400, h=300)
+DISPLAY = pi3d.Display.create(x=50, y=50)
 shader = pi3d.Shader("shaders/2d_flat")
-sprite = pi3d.ImageSprite("textures/PATRN.PNG", shader)
+sprite = pi3d.ImageSprite("textures/PATRN.PNG", shader, w=10.0, h=10.0, z=1.0)
 
+mykeys = pi3d.Keyboard()
 while DISPLAY.loop_running():
   sprite.draw()
+  
+  if mykeys.read() == 27:
+    mykeys.close()
+    DISPLAY.destroy()
+    break
 
--- demos/Pi3d2.py
+++ demos/Pi3d2.py
@@ -1,95 +1,29 @@
 #!/usr/bin/python
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-import math,random,time
-
 import demo
+import pi3d
 
-from pi3d.constants import *
+DISPLAY = pi3d.Display.create(x=50, y=50)
+shader = pi3d.Shader("shaders/star")
+tex = pi3d.Texture("textures/PATRN.PNG")
+box = pi3d.Cuboid(x=0, y=0, z=1.5)
+box.set_draw_details(shader,[tex])
+tm = 0.0
+dt = 0.01
+sc = 0.0
+ds = 0.001
 
-from pi3d import Display
-from pi3d.Texture import Texture
-from pi3d.Keyboard import Keyboard
-
-from pi3d.Light import Light
-from pi3d.Shader import Shader
-
-from pi3d.shape.Sphere import Sphere
-from pi3d.util.String import String
-from pi3d.util.Ttffont import Ttffont
-from pi3d.shape.MergeShape import MergeShape
-
-# Setup display and initialise pi3d
-DISPLAY = Display.create(x=10, y=10, w=1000, h=800)
-DISPLAY.set_background(0.4, 0.6, 0.8, 1.0)      # r,g,b,alpha
-
-#setup textures, light position and initial model position
-Light((0, 5, 0))
-#create shaders
-shader = Shader("shaders/uv_reflect")
-flatsh = Shader("shaders/uv_flat")
-
-#Create textures
-shapeimg = Texture("textures/straw1.jpg")
-shapebump = Texture("textures/floor_nm.jpg", True)
-shapeshine = Texture("textures/pong3.png")
-
-#Create shape
-myshape = MergeShape()
-num = (2, 2)
-asphere = Sphere(sides=32)
-for i in range(num[0]):
-  for j in range(num[1]):
-    myshape.add(asphere, -num[0]*0.9 + 1.8*i, -num[1]*0.9 +1.8*j, 0.0)
-
-myshape.position(0.0, 0.0, 5)
-myshape.set_draw_details(shader, [shapeimg, shapebump, shapeshine], 8.0, 0.0, 2.0, 2.0)
-#light2 = Light((-5, -5, i - 3))
-#myshape.set_light(light2)
-
-cAngle = [0.0, 0.0, 0.0]
-dx = 0.05
-
-tick=0
-next_time = time.time()+10.0
-
-arialFont = Ttffont("fonts/FreeMonoBoldOblique.ttf", "#dd00aa")   #load ttf font and set the font colour to 'raspberry'
-mystring = String(font=arialFont, string="Now the Raspberry Pi really does rock")
-mystring.translate(0.0, 0.0, 1)
-mystring.set_shader(flatsh)
-
-# Fetch key presses.
-mykeys = Keyboard()
-
-# Display scene and rotate shape
+mykeys = pi3d.Keyboard()
 while DISPLAY.loop_running():
-
-  #camera.reset()
-  #cAngle[1] += 0.5
-  #camera.rotateY(cAngle[1])
-
-  myshape.draw()
-  myshape.rotateIncY(0.247)
-  myshape.rotateIncZ(0.1613)
-  myshape.translateX(dx)
-  if myshape.x() > 5: dx = -0.05
-  elif myshape.x() < -5: dx = 0.05
-
-
-  mystring.draw()
-  mystring.rotateIncZ(0.05)
-
-  #camera.was_moved = False
-
-  if time.time() > next_time:
-    print("FPS:",tick/10.0)
-    tick=0
-    next_time = time.time()+10.0
-  tick+=1
+  box.set_custom_data(48, [tm, sc, -0.5 * sc])
+  tm += dt
+  sc = (sc + ds) % 10.0
+  box.rotateIncX(0.01)
+  box.rotateIncY(0.071)
+  box.draw()
 
   if mykeys.read() == 27:
     mykeys.close()
     DISPLAY.destroy()
     break
-
-quit()
