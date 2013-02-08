c6f6dd94ed6f0341dfd76906606f12a129d3e610
Tom Ritchford <tom@swirly.com>
2013-02-07 20:08:36 -0500

    Fix up comment in Sprite.py

--- pi3d/shape/Sprite.py
+++ pi3d/shape/Sprite.py
@@ -11,15 +11,15 @@ class Sprite(Shape):
                rx=0.0, ry=0.0, rz=0.0,
                sx=1.0, sy=1.0, sz=1.0,
                cx=0.0, cy=0.0, cz=0.0):
-    """uses standard constructor for Shape.E xtra Keyword arguments:
-    
+    """Uses standard constructor for Shape. Extra Keyword arguments:
+
       *w*
         Width.
       *h*
         Height.
-    """  
+    """
     super(Sprite, self).__init__(camera, light, name, x, y, z, rx, ry, rz,
-                                sx, sy, sz, cx, cy, cz)
+                                 sx, sy, sz, cx, cy, cz)
     self.width = w
     self.height = h
     self.ttype = GL_TRIANGLES
