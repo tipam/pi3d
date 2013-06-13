cb3082480d18273e05763c064e0442d87e6fb9e2
Patrick Gaunt <patrick@eldwick.org.uk>
2013-06-13 12:31:00 +0000

    screen back to useable size

--- demos/DogFight.py
+++ demos/DogFight.py
@@ -7,7 +7,7 @@ import demo
 import pi3d
 
 #display, camera, shader
-DISPLAY = pi3d.Display.create(x=50, y=50, w=800, h=800, frames_per_second=20)
+DISPLAY = pi3d.Display.create(x=100, y=100, frames_per_second=20)
 #a default camera is created automatically but we might need a 2nd 2D camera
 #for displaying the instruments etc. Also, because the landscape is large
 #we need to set the far plane to 10,000
