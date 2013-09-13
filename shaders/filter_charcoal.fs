ed18d5c561e5b98b4349d73835976ed0c9f045a7
paddywwoof <patrick@eldwick.org.uk>
2013-09-13 12:10:49 +0100

    charcoal shader picks up shade from unif[16] (48:51 in python)

--- shaders/filter_charcoal.fs
+++ shaders/filter_charcoal.fs
@@ -14,7 +14,8 @@ void main(void){
   vec4 c3 = texture2D(tex0, uv + vec2(0, clamp(d.y, -1.0, 0.0)));
   float f = distance(c1.rgb, c2.rgb) + distance(c1.rgb, c3.rgb) - 0.2;
   f = clamp(f, 0.0, 1.0);
-  c1.rgb = vec3(1.0, 1.0, 1.0) * (1.0 - f);
+  //c1.rgb = (vec3(1.0, 1.0, 1.0) - unif[16]) * (1.0 - f) + vec3(unif[16]) * f; // negative paper
+  c1.rgb = vec3(1.0, 1.0, 1.0) * (1.0 - f) + vec3(unif[16]) * f; // white paper
   gl_FragColor = c1;
   gl_FragColor.a *= unif[5][2];
 }
