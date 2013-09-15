3970248ce42c3dc70611d389245d90e8a6db9bcc
paddywwoof <patrick@eldwick.org.uk>
2013-09-15 22:22:29 +0100

    oops, Display dimension should be full screen

--- Slideshow_2d.py
+++ Slideshow_2d.py
@@ -53,7 +53,7 @@ LOGGER = pi3d.Log.logger(__name__)
 LOGGER.info("Log using this expression.")
 
 # Setup display and initialise pi3d
-DISPLAY = pi3d.Display.create(w=1024, h=766, background=(0.0, 0.0, 0.0, 1.0), frames_per_second=20)
+DISPLAY = pi3d.Display.create(background=(0.0, 0.0, 0.0, 1.0), frames_per_second=20)
 shader = pi3d.Shader("2d_flat")
 
 #iFiles = glob.glob("/home/pi/slidemenu/testdir/*.*")
