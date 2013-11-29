f0af5c37a5fa53377d3f8cdfc387a7361c008f6d
paddywwoof <patrick@eldwick.org.uk>
2013-11-29 18:41:12 +0000

    added some model manipulation so nearly as per @mung prototype

--- Gui.py
+++ Gui.py
@@ -14,18 +14,71 @@ def cbx(*args):
 def cb(*args):
   print(args)
 
+class scroll_cb(object):
+  def __init__(self, callback, delta):
+    self.callback = callback
+    self.delta = delta
+
+  def roty(self, *args):
+    print(args)
+    slideval = args[0] * self.delta
+    self.callback(slideval)
+  
+class jogger(object):
+  "jogger class"
+  def __init__(self, gui, label, x, y, callback, delta) :
+    self.callback = callback
+    self.delta = delta
+    self.x = x
+    self.y = y
+    self.butP = pi3d.Button(gui, "l.gif", x, y, label=label,
+                                        shortcut='d', callback=self.rotp)
+    self.butM = pi3d.Button(gui, "r.gif", x + 32, y, shortcut='l',
+                                        callback=self.rotm)
+
+  def rotp(self, *args):
+    self.callback(self.delta)
+
+  def rotm(self, *args):
+    self.callback(-self.delta)
+
 DISPLAY = pi3d.Display.create(w=640, h=480, frames_per_second=30)
 DISPLAY.set_background(0.8,0.8,0.8,1.0) # r,g,b,alpha
 
+shader = pi3d.Shader("uv_reflect")
 font = pi3d.Font("fonts/FreeSans.ttf", color=(0,0,0,255), font_size=20)
 gui = pi3d.Gui(font)
 ww, hh = DISPLAY.width / 2.0, DISPLAY.height / 2.0
-radio = pi3d.Radio(gui, ww -20, hh - 10,
-                label="This unhides the menu!", label_pos="left", callback=cbx)
+
+img = pi3d.Texture("textures/rock1.jpg")
+model = pi3d.Cuboid(z=5.0)
+model.set_draw_details(shader, [img])
+
+radio = pi3d.Radio(gui, ww -20, hh - 32,
+                label="unhides menu!", label_pos="left", callback=cbx)
+xi = -ww
+yi = hh
+for b in ['tool_estop.gif', 'tool_power.gif', 'tool_open.gif',
+          'tool_reload.gif', 'tool_run.gif', 'tool_step.gif',
+          'tool_pause.gif', 'tool_stop.gif', 'tool_blockdelete.gif',
+          'tool_optpause.gif', 'tool_zoomin.gif', 'tool_zoomout.gif',
+          'tool_axis_z.gif', 'tool_axis_z2.gif', 'tool_axis_x.gif',
+          'tool_axis_y.gif', 'tool_axis_p.gif', 'tool_rotate.gif',
+          'tool_clear.gif']:
+  g = pi3d.Button(gui, b, xi, yi, shortcut='d', callback=cb)
+  xi = xi + 32
+
+
 button = pi3d.Button(gui, ["tool_run.gif", "tool_pause.gif"], ww - 40,
                 -hh + 40, callback=cb, shortcut='q')
+scr_cb = scroll_cb(model.rotateToY, -360) #convoluted way of avoiding global!
 scrollbar = pi3d.Scrollbar(gui, -ww + 20, -hh + 20, 200, start_val=50,
-                label="slide me", label_pos='above', callback=cb)
+                label="slide me", label_pos='above', callback=scr_cb.roty)
+
+jog1 = jogger(gui, 'X', ww - 64, hh - 64, model.translateX, -0.1)
+jog2 = jogger(gui, 'Y', ww - 64, hh - 96, model.translateY, 0.1)
+jog3 = jogger(gui, 'Z', ww - 64, hh - 128, model.translateZ, 0.1)
+jog4 = jogger(gui, 'Zt', ww - 64, hh - 160, model.rotateIncZ, 7)
 
 mi1 = pi3d.MenuItem(gui,"File")
 mi2 = pi3d.MenuItem(gui,"Edit")
@@ -38,12 +91,12 @@ mi121 = pi3d.MenuItem(gui, "v3", callback=cb)
 mi122 = pi3d.MenuItem(gui, "v4", callback=cb)
 
 menu1 = pi3d.Menu(parent_item=None, menuitems=[mi1, mi2, mi3],
-          x=-DISPLAY.width/2, y=DISPLAY.height/2, visible=True)
+          x=-ww, y=hh-32, visible=True)
 menu2 = pi3d.Menu(parent_item=mi1, menuitems=[mi11, mi12], horiz=False, position='below')
 menu3 = pi3d.Menu(parent_item=mi11, menuitems=[mi111, mi112], horiz=False, position='right')
 menu4 = pi3d.Menu(parent_item=mi12, menuitems=[mi121, mi122], horiz=False, position='right')
 
-textbox = pi3d.TextBox(gui, "type here", 100, 100, callback=cb, label='TextBox (KEY t to edit)',
+textbox = pi3d.TextBox(gui, "type here", 100, -180, callback=cb, label='TextBox (KEY t to edit)',
                         shortcut='t')
 
 mx, my = 0, 0
@@ -54,6 +107,7 @@ while DISPLAY.loop_running() and not inputs.key_state("KEY_ESC"):
   imx, imy, mv, mh, butt = inputs.get_mouse_movement()
   mx += imx
   my -= imy
+  model.draw()
   gui.draw(mx, my)
   if inputs.key_state("BTN_MOUSE"):
     gui.check(mx, my)
