3e0fadd0cd76b65c9fc65d8bd45e09ab46830fb3
admin <paddywwoof>
2014-10-05 18:39:42 +0100

    calcHeight returns correct normal for use in surface following animations

--- pi3d/shape/ElevationMap.py
+++ pi3d/shape/ElevationMap.py
@@ -7,7 +7,8 @@ from six.moves import xrange
 
 from PIL import Image, ImageOps
 
-from numpy import cross, dot, sqrt, array, arctan2, arcsin, degrees, subtract
+from numpy import cross, dot, array, sum
+from math import atan2, asin, degrees, sqrt
 
 from pi3d import *
 from pi3d.Buffer import Buffer
@@ -176,12 +177,14 @@ class ElevationMap(Shape):
 
     return pixht + self.unif[1]
 
-  def calcHeight(self, px, pz):
-    """accurately return the hight of the map at the point specified
+  def calcHeight(self, px, pz, inc_normal=False):
+    """accurately return the height of the map at the point specified
 
     Arguments:
       *px, pz*
         Location of the point in world coordinates to calculate height.
+      *inc_normal*
+        optionall return a tuple with height and normal vector (h, (nx,ny,nz))
     """
     #adjust for map not set at origin
     px -= self.unif[0]
@@ -215,13 +218,14 @@ class ElevationMap(Shape):
 
     if pz > (v0[2] + hs / ws * (px - v0[0])):
     #opposite side of the diagonal so swap base corners
-      v_swap = v2
+      y, n = _intersect_triangle(v2, v3, v0, (px, 0, pz))
     else:
-      v_swap = v1
-    return self.unif[1] + intersect_triangle(v_swap, v0, v3, (px, 0, pz))
+      y, n = _intersect_triangle(v1, v0, v3, (px, 0, pz))
+    if inc_normal:
+      n /= ((n * n).sum()) ** 0.5 # normalise to unit len
+      return (self.unif[1] + y, n)
+    return self.unif[1] + y
 
-  # TODO these functions will be scrambled by any scaling, rotation or offset,
-  #either print warning or stop these operations applying
   def clashTest(self, px, py, pz, rad):
     """Works out if an object at a given location and radius will overlap
     with the map surface. Returns four values:
@@ -256,11 +260,10 @@ class ElevationMap(Shape):
     if z0 < 0: z0 = 0
     z1 = int(math.floor((halfd + pz + rad)/dz + 0.5)) + 1
     if z1 > self.iy-1: z1 = self.iy-1
-
     # go through grid around px, pz
     minDist, minLoc = 1000000, (0, 0)
-    for i in xrange(x0+1, x1):
-      for j in xrange(z0+1, z1):
+    for i in range(x0+1, x1):
+      for j in range(z0+1, z1):
         # use the locations stored in the one dimensional vertices matrix
         #generated in __init__. 3 values for each location
         p = j*self.ix + i # pointer to the start of xyz for i,j in the vertices array
@@ -294,7 +297,7 @@ class ElevationMap(Shape):
             minDist = pDistSq
             minLoc = (i,j)
 
-    gLevel = self.calcHeight(px, pz) #check it hasn't tunnelled through by going fast
+    gLevel = self.calcHeight(px + self.unif[0], pz + self.unif[1]) #check it hasn't tunnelled through by going fast
     if gLevel > (py-rad):
       minDist = py - gLevel
       minLoc = (int((x0+x1)/2), int((z0+z1)/2))
@@ -343,7 +346,7 @@ class ElevationMap(Shape):
     forwd = array([-normp[2]*normp[0], -normp[2]*normp[1],
                   normp[0]*normp[0] + normp[1]*normp[1]])
     forwd = forwd / sqrt(forwd.dot(forwd))
-    return (degrees(arcsin(-forwd[1])), degrees(arctan2(sidev[1], normp[1])))
+    return (degrees(asin(-forwd[1])), degrees(atan2(sidev[1], normp[1])))
 
   def __getstate__(self): # to allow pickling
     self.childModel = None
@@ -363,7 +366,7 @@ class ElevationMap(Shape):
     self.ix = state['ix']
     self.iy = state['iy']
 
-def intersect_triangle(v1, v2, v3, pos):
+def _intersect_triangle(v1, v2, v3, pos):
   """calculates the y intersection of a point on a triangle and returns the y
   value of the intersection of the line defined by x,z of pos through the
   triange defined by v1,v2,v3
@@ -375,8 +378,8 @@ def intersect_triangle(v1, v2, v3, pos):
       tuple (x,y,z) defining the x,z of the vertical line intersecting triangle
   """
   #calc normal from two edge vectors v2-v1 and v3-v1
-  nVec = cross(subtract(v2, v1), subtract(v3, v1))
+  nVec = cross((v2 - v1), (v3 - v1))
   #equation of plane: Ax + By + Cz = kVal where A,B,C are components of normal. x,y,z for point v1 to find kVal
-  kVal = dot(nVec,v1)
-  #return y val i.e. y = (kVal - Ax - Cz)/B
-  return (kVal - nVec[0]*pos[0] - nVec[2]*pos[2])/nVec[1]
+  kVal = dot(nVec, v1)
+  #return y val i.e. y = (kVal - Ax - Cz)/B also the normal vector seeing as this has been calculated
+  return (kVal - nVec[0]*pos[0] - nVec[2]*pos[2]) / nVec[1], nVec
