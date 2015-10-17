44b83b5b22a72edc3b8509608ad3c1f0df43e436
admin <patrick@eldwick.org.uk>
2015-10-17 14:33:01 +0100

    bug fix, u prefix works py2 and 3.3 but not py3.2. also tidying code

--- pi3d/util/FastText.py
+++ pi3d/util/FastText.py
@@ -4,9 +4,7 @@ import numpy as np
 from pi3d.shape.Points import Points
 from pi3d.Shader import Shader
 from pi3d.util.TextBlock import TextBlock
-#from gettattra import *
-#from numpy.f2py.auxfuncs import throw_error
-#from __builtin__ import str
+
 import math
 import colorsys
 
@@ -23,18 +21,17 @@ class FastText(object):
     """
     self.max_chars = max_chars
     self.font = font
-
     self.shader = Shader("uv_pointsprite")
     
     self.locations = np.zeros((max_chars, 3))
     # :,2 for size and z offset. 
-    # size=fract(location[2] range 0.0 to 0.99)
-    # zoffset = (location[2]-size)*0.1
+    # size=fract(location[2] range 0.0 to 0.999)
+    # zoffset = (location[2] - size)*0.1
 
     self.normals = np.zeros((max_chars, 3))
     # :,0 for rotation
-    # :,1 for red and green, red=normal[1]/256, green=fract(normal[1])
-    # :,2 for blue and alpha, blue=normal[2]/256, alpha=fract(normal[2])    
+    # :,1 for red and green, red=normal[1]/999, green=fract(normal[1])
+    # :,2 for blue and alpha, blue=normal[2]/999, alpha=fract(normal[2])    
     self.normals[:,1] = 0.0
     self.normals[:,2] = 0.0
     self.uv = np.zeros((max_chars, 2)) # u picnum.u v
@@ -47,15 +44,16 @@ class FastText(object):
                    point_size=self.font.height)
     self.text.set_draw_details(self.shader, [self.font])
     self.text.unif[48] = 0.057  # used to hold
-
     #Reset all characters to space so there are no false character shadows
-    glyph = self.font.glyph_table[u' ']
+    try:
+      glyph = self.font.glyph_table[' '] #u' ' doesn't work on python3.2!!
+    except:
+      glyph = list(self.font.glyph_table.values())[0] #in case ' ' isn't there
     self.uv[:] = glyph[0:2]
 
-
   def regen(self):
-    ''' Regenerate all text blocks that are linked to data objects and have changed value
-    '''
+    ''' Regenerate all text blocks that are linked to data objects and have
+    changed value '''
     for block in self.text_blocks:
       if block.data_obj != None:
         value = block.get_value()
@@ -63,13 +61,11 @@ class FastText(object):
           block.last_value = value
           block.set_text()
 
-
   def add_text_block(self, text_block):
-    ''' Add a text block to the collection, setting the object link to this service and setting
-        the buffer offset allocated to the text block.  This is required for the text block to
-        update the buffer allocated to it. Also tracks the next unallocated character in the
-        buffer.
-    '''
+    ''' Add a text block to the collection, setting the object link to this
+    service and setting the buffer offset allocated to the text block.
+    This is required for the text block to update the buffer allocated to
+    it. Also tracks the next unallocated character in the buffer. '''
     if self._first_free_char + text_block.char_count >= self.max_chars:
       print("failed to allocate space in characers for " + text_block.char_count + " characters")
       return -1
@@ -78,16 +74,14 @@ class FastText(object):
     text_block.set_text()
     self._first_free_char += text_block.char_count
     return 0
-  
-  
+
   def draw(self):
-    ''' Draw all the text characters.  If the re_init flag is set then update the points shape buffer.
-    '''
+    ''' Draw all the text characters.  If the re_init flag is set then
+    update the points shape buffer. '''
     if self._do_buffer_reinit == True:
         self.text.buf[0].re_init(pts=self.locations, normals=self.normals, texcoords=self.uv) # reform opengles array_buffer
 
     self.text.draw()
-
 
   def set_do_reinit(self):
     self._do_buffer_reinit = True
--- pi3d/util/TextBlock.py
+++ pi3d/util/TextBlock.py
@@ -3,12 +3,8 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 """ Text block used to update contents of buffers used to draw gl_point font characters.
 """
 import numpy as np
-
-#from gettattra import *
-#from __builtin__ import str
 import math
 import colorsys
-#import FastTextColoured as FastTextColoured
 
 def getattra(obj, attr, default):
     if("[" in attr):
@@ -19,17 +15,21 @@ def getattra(obj, attr, default):
             return thearray[index]
         else:
             return default
-        
     else:
         return getattr(obj, attr, default)
 
+
 class TextBlock(object):
-  def __init__(self, x, y, z, rot, char_count, data_obj, attr, text_format="{:s}", size=0.25, spacing="C", space=1.1, colour=(1.0,1.0,1.0,1.0) , char_rot=0.0, justify=0.0):
+  def __init__(self, x, y, z, rot, char_count, data_obj=None, attr=None,
+            text_format="{:s}", size=0.25, spacing="C", space=1.1,
+            colour=(1.0,1.0,1.0,1.0) , char_rot=0.0, justify=0.0):
     """ Arguments:
     *x, y, z*:
       As usual
     *rot*:
       rotation in degrees
+    *char_count*:
+      number of characters for this block (capacity it can expand into)
     *data_obj*:
       Data object to use in text format
     *attr*:
@@ -63,79 +63,69 @@ class TextBlock(object):
     self.colour = [colour[0],colour[1],colour[2],colour[3]]
     self.char_rot = math.radians(char_rot)
     self.justify = justify
- 
-    self.last_value = self      # hack so that static None object get initialization
+    self.last_value = self # hack so that static None object get initialization
     self.rotation_changed = False
-    
     self._buffer_index = 0
     self._text_manager = None
     self._string_length = 0
-    
-    self.char_offsets = [0.0]*char_count
-    
-    str = self.get_string(self.get_value())
-    self._string_length = len(str)
-    
+    self.char_offsets = [0.0] * char_count
+    self._string_length = len(self.get_string(self.get_value()))
 
   def set_text_manager(self, manager, buffer_index):
     self._text_manager = manager
     self._buffer_index = buffer_index
-    
+
   def get_value(self):
-    if(self.attr != None) and (self.data_obj != None):
+    if (self.attr is not None) and (self.data_obj is not None):
       return getattra(self.data_obj, self.attr, None)
     return None
-    
-  def get_string(self, value):    
-    if(value != None):
-      str = self.text_format.format(value)
-      self._string_length = len(str)
-      return str
 
+  def get_string(self, value):    
+    if value is not None:
+      strval = self.text_format.format(value)
+      self._string_length = len(strval)
+      return strval
     return self.text_format
-  
+
   def set_position(self, x=None, y=None, z=None, rot=None):
-    if x != None: self.x = x
-    if y != None: self.y = y
-    if z != None: self.z = z
-    if rot != None: self.rot = math.radians(rot)
-    
+    if x is not None:
+      self.x = x
+    if y is not None:
+      self.y = y
+    if z is not None:
+      self.z = z
+    if rot is not None:
+      self.rot = math.radians(rot) 
+
     size_pos, __  = math.modf(self.size)
     size_pos += math.trunc(self.z * 10.0)   # depth has resolution of 0.1m and range of 25.5m
     pos = [self.x, self.y, size_pos]
-    
+
     locations = np.zeros((self.char_count, 3), dtype=np.float)
     locations[:, 0] = np.multiply(self.char_offsets, math.cos(self.rot))
     locations[:, 1] = np.multiply(self.char_offsets, math.sin(self.rot))
     locations = np.add(locations, pos)
-    self._text_manager.locations[self._buffer_index:self._buffer_index+self.char_count, :] = locations
-    
-    self._text_manager.normals[self._buffer_index:self._buffer_index+self._string_length, 0] = self.rot + self.char_rot
-
+    self._text_manager.locations[self._buffer_index:self._buffer_index + self.char_count, :] = locations
+    self._text_manager.normals[self._buffer_index:self._buffer_index + self._string_length, 0] = self.rot + self.char_rot
     self._text_manager.set_do_reinit()
-    
-    
+
   def set_colour(self, colour=None, alpha=None):
-    if colour != None:
+    if colour is not None:
       self.colour[0:2] = colour[0:2]
-    
-    if alpha != None:
-      self.colour[3] = alpha
-      
+    if alpha is not None:
+      self.colour[3] = alpha 
     #Reset alpha to zero for all characters.  Prevents displaying old chars from longer strings
-    self._text_manager.normals[self._buffer_index:self._buffer_index+self.char_count, 1] = 0
-        
+    self._text_manager.normals[self._buffer_index:self._buffer_index + self.char_count, 1] = 0
+
     #Fill an array with the colour to copy to the manager normals
     #rotation is included for efficiency
     normal = np.zeros((3), dtype=np.float)
     normal[0] = self.rot + self.char_rot
     normal[1] = (self.colour[1] * 0.999) + (math.floor(self.colour[0] * 999))
     normal[2] = (self.colour[3] * 0.999) + (math.floor(self.colour[2] * 999))
-    
     #Only set colour alpha for string length. Zero for non displayed characters
     self._text_manager.normals[self._buffer_index:self._buffer_index+self._string_length, :] = normal
-    
-    
+
   def set_colour_gradient(self, colour1, colour2, alpha1=None, alpha2=None):
     ''' Colour each character with a gradient from colour1 to colour2
     Interpolate hsv instead of rgb since it is a more natural change.
@@ -144,15 +134,13 @@ class TextBlock(object):
     '''
     hsv1 = colorsys.rgb_to_hsv(colour1[0], colour1[1], colour1[2])
     hsv2 = colorsys.rgb_to_hsv(colour2[0], colour2[1], colour2[2])
-      
     normal = np.zeros((3), dtype=np.float)
     normal[0] = self.rot + self.char_rot
-    
-    if alpha1 == None:
+    if alpha1 is None:
       alpha1 = self.colour[3]
-    if alpha2 == None:
-      alpha2 = alpha1
-    
+    if alpha2 is None:
+      alpha2 = alpha1 
+
     for index in range(0,self._string_length):
       h = np.interp(index, [0,self._string_length], [hsv1[0], hsv2[0]])
       s = np.interp(index, [0,self._string_length], [hsv1[1], hsv2[1]])
@@ -161,30 +149,32 @@ class TextBlock(object):
       rgb = colorsys.hsv_to_rgb(h, s, v)
       normal[1] = (rgb[0] * 999) + (rgb[1] * 0.999)
       normal[2] = (rgb[2] * 999) + (a * 0.999)
-      
       #Only set colour alpha for string length. Zero for non displayed characters
-      self._text_manager.normals[self._buffer_index+index, :] = normal
-     
-    
-  def set_text(self, text_format=None, size=None, spacing=None, space=None , char_rot=None, set_pos=True, set_colour=True):
-    if text_format != None: self.text_format = text_format
-    if size != None: self.size = size
-    if spacing != None: self.spacing = spacing
-    if space != None: self.space = space
-    if char_rot != None: self.char_rot = math.radians(char_rot)
-        
+      self._text_manager.normals[self._buffer_index + index, :] = normal
+
+  def set_text(self, text_format=None, size=None, spacing=None, space=None,
+              char_rot=None, set_pos=True, set_colour=True):
+    if text_format is not None:
+      self.text_format = text_format
+    if size is not None:
+      self.size = size
+    if spacing is not None:
+      self.spacing = spacing
+    if space is not None:
+      self.space = space
+    if char_rot is not None:
+      self.char_rot = math.radians(char_rot)
     #If there is no data object then use the simple static string value
-    if (self.data_obj != None):
+    if self.data_obj is not None:
       value = self.get_value()
-      str = self.get_string(value)
+      strval = self.get_string(value)
       self.last_value = value
     else:
-      str = self.text_format
-    self._string_length =len(str)
-     
+      strval = self.text_format
+    self._string_length =len(strval)
+
     pos = 0.0
     index = 0
-    
     const_width = 0.0
     vari_width = 0.0
     if self.spacing == "C":
@@ -194,30 +184,23 @@ class TextBlock(object):
     if self.spacing == "F":
       vari_width = self.size
       const_width =  (64.0 * self.space * self.size)    
-    
-    for char in str:
+
+    for char in strval:
       glyph = self._text_manager.font.glyph_table[char]
       self._text_manager.uv[index+self._buffer_index] = glyph[4:]
-        
       char_offset = pos
-      
       if self.spacing == "F":
         #center character to the right 
         char_offset += float(glyph[0]) * self.size * 0.5
-      
       self.char_offsets[index] = char_offset
-
       spacing = (glyph[0] * vari_width) + const_width
       pos += spacing
       index += 1
     
     #Justification
     self.char_offsets = np.add(self.char_offsets, (pos - spacing) * -self.justify)     
-    
     if set_pos:
       self.set_position()
-    
     if set_colour:
       self.set_colour()
-      
-    self._text_manager.set_do_reinit()  
+    self._text_manager.set_do_reinit()
