4030971a1c825f010fb213da7577e276355a5d0e
paddywwoof <patrick@eldwick.org.uk>
2017-07-27 23:09:21 +0100

    version number nudge

--- pi3d/constants/__init__.py
+++ pi3d/constants/__init__.py
@@ -5,7 +5,7 @@ pi3d.constants contains constant values, mainly integers, from OpenGL ES 2.0.
 """
 import time
 
-__version__ = '2.19'
+__version__ = '2.20'
 year = time.localtime().tm_year
 
 STARTUP_MESSAGE = """
