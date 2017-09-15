c6f240b96c402848821592f914f5886ccaa6d0ac
paddywwoof <patrick@eldwick.org.uk>
2017-09-15 10:11:59 +0100

    missed y component of gravity in acc check

--- pi3d/util/PexParticles.py
+++ pi3d/util/PexParticles.py
@@ -81,10 +81,10 @@ class PexParticles(Points):
     self.rot_rate = rot_rate
     self.rot_var = rot_var
     # make a flag to avoid this expensive operation if no accelerators
-    self.any_acceleration = (self.gravity['x'] != 0.0 or 
+    self.any_acceleration = (self.gravity['x'] != 0.0 or self.gravity['y'] != 0.0 or
                              self.radialAcceleration != 0.0 or
                              self.tangentialAcceleration != 0.0)
-    
+    print(self.any_acceleration)
     ''' Buffer.array_buffer holds
     [0] vertices[0] x position of centre of point relative to centre of screen in pixels
     [1] vertices[1] y position
