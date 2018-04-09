95ac1818d3ddfefeff06ba926f1e262bb3f90bf6
paddywwoof <patrick@eldwick.org.uk>
2018-04-09 15:56:29 +0100

    minor bug fix release

--- pi3d/constants/__init__.py
+++ pi3d/constants/__init__.py
@@ -5,7 +5,7 @@ pi3d.constants contains constant values, mainly integers, from OpenGL ES 2.0.
 """
 import time
 
-__version__ = '2.23'
+__version__ = '2.24'
 year = time.localtime().tm_year
 
 STARTUP_MESSAGE = """
